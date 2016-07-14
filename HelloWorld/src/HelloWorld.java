63acc3fab6107e98b8bdefb3e493398ba0998a72
ChenSammi <sammi.chen@intel.com>
2016-07-14 20:50:57 +0800

    add hello DCM

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello Shanghai");
 		System.out.println("Hello Zizhu");
+		System.out.println("Hello DCM");
 	}
 
 }
